1854b7cc636ea83425e4e094bc5f4e8297cecc05
JP3Pe <rlaworhkd430430@gmail.com>
2022-02-18 16:39:56 +0900

    up: Change background image from HTML to CSS

--- js/background.js
+++ js/background.js
@@ -2,8 +2,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bodyElement = document.body;
+const backgroundImagePath = `url(img/${chosenImage})`;
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+bodyElement.style["backgroundImage"] = backgroundImagePath;
